f4b5f9d2abe7b3ba6700138f6901db3b62a62c6b
YaLove2cIt <135716112+YL2CI@users.noreply.github.com>
2023-06-07 10:05:36 -0400

    bookmarklet.js
    6/7/2023. 10:05 est usa

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(){var%20s=document.createElement('script');s.src='https://your-website.com/autoclicker.js';document.body.appendChild(s);})();
+javascript:(function(){var%20s=document.createElement('script');s.src='https://github.com/YL2CI/autoclicker/blob/main/autoclicker.js';document.body.appendChild(s);})();
