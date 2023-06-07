94b8727ecd4fc521e6e0f8de41c8e4b3b2a68011
YaLove2cIt <135716112+YL2CI@users.noreply.github.com>
2023-06-07 09:56:14 -0400

    autoclicker.js
    6/7/2023. 9:56 est usa

---
+++ autoclicker.js
@@ -0,0 +1,4 @@
+javascript:(function() {
+    var button = document.querySelector('button'); // select the button element you want clicked
+    button.click(); // simulate a click event on the button
+})();
